71d73c630f3fc891c229e378aab0ad5007f37761
Adrian Schimpf <adrian@aivie.ch>
2023-02-26 17:33:50 +0100

    use newest version of cke via npm

--- app/bundles/CoreBundle/Templating/Helper/AssetsHelper.php
+++ app/bundles/CoreBundle/Templating/Helper/AssetsHelper.php
@@ -502,7 +502,7 @@ class AssetsHelper
 
     private function getCKEditorScripts(): array
     {
-        $base    = 'app/bundles/CoreBundle/Assets/js/libraries/ckeditor/';
+        $base    = 'node_modules/ckeditor4/';
 
         return [
             $base.'ckeditor.js?v'.$this->version,
